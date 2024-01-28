ddee8a7907566a03614574b2e81b473070d8eda4
Chik <Tolstykh2004VS@gmail.com>
2024-01-28 12:15:32 +0500

    Add project files.

---
+++ Tyuiu.TolstykhVS.Sprint4.Task0.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TolstykhVS.Sprint4.Task0.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TolstykhVS.Sprint4.Task0.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b83b6149-b1a7-4af4-825c-5fe5db98f597")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.TolstykhVS.Sprint4.Task0.V17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TolstykhVS.Sprint4.Task0.V17.Lib;
+
+namespace Tyuiu.TolstykhVS.Sprint4.Task0.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Толстых В.С | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерный статический массив                                     *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #17                                                              *");
+            Console.WriteLine("* Выполнил: Толстых В.С | СМАРТб-23-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму          *");
+            Console.WriteLine("* четных элементов массива.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("{4 ,8 ,7 ,6 ,5 ,8 ,2 ,4 ,3 ,2}");
+            int[] Array = new int[] { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.GetSumEvenArrEl(Array);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
