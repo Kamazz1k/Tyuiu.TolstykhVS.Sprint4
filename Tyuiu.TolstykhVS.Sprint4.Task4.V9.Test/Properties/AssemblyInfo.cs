17bee35d0883007c1867546cea6a86ce14bf31a1
Kamazz1k <144707479+Kamazz1k@users.noreply.github.com>
2024-01-28 15:04:11 +0500

    Task4

---
+++ Tyuiu.TolstykhVS.Sprint4.Task4.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TolstykhVS.Sprint4.Task4.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TolstykhVS.Sprint4.Task4.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4517ce32-97de-45e9-89f1-1667b04dd9d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
