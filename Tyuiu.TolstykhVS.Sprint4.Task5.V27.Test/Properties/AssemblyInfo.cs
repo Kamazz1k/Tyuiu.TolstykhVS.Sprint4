978f93d2d9f919226162e475af3357fd1d48d7b1
Kamazz1k <144707479+Kamazz1k@users.noreply.github.com>
2024-01-28 17:16:46 +0500

    Task5

---
+++ Tyuiu.TolstykhVS.Sprint4.Task5.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TolstykhVS.Sprint4.Task5.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TolstykhVS.Sprint4.Task5.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8cf0c135-08d0-455b-8856-a2e90ed10ba7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
